4c751e826ac1a951539faeebb55537f1e64d6789
kmmtechnology <pronay_kumar_das_0205082@yahoo.com>
2016-02-20 12:57:10 +0600

    android app
    android app pushing

---
+++ app/src/main/java/com/demo/messenger/MainActivity.java
@@ -0,0 +1,23 @@
+package com.demo.messenger;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+   private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=(TextView)findViewById(R.id.textView);
+
+        Typeface typeface=Typeface.createFromAsset(getAssets(),getString(R.string.font_path));
+        textView.setTypeface(typeface);
+    }
+
+
+}
